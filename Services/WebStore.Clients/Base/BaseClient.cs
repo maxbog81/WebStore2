376e2db1f2173da118473aba133dadcc60a04a8f
Maksim Bogatov <maxbog@inbox.ru>
2020-02-18 21:40:51 +0300

    Урок 1

---
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+
+namespace WebStore.Clients.Base
+{
+    public abstract class BaseClient : IDisposable
+    {
+        protected readonly HttpClient _Client;
+
+        protected readonly string _ServiceAddress;
+
+        protected BaseClient(IConfiguration config, string ServiceAddress)
+        {
+            _ServiceAddress = ServiceAddress;
+
+            _Client = new HttpClient
+            {
+                BaseAddress = new Uri(config["WebApiURL"])
+            };
+
+            var headers = _Client.DefaultRequestHeaders.Accept;
+            headers.Clear();
+            headers.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+
+        #region IDisposable
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_Disposed || !disposing) return;
+            _Disposed = true;
+            _Client.Dispose();
+        } 
+
+        #endregion
+    }
+}
---
+++ Services/WebStore.ServiceHosting/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Enumerable.Range(1, 22).Select(i => $"value{i}").ToArray();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
