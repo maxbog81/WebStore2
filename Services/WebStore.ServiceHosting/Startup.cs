e5dbb78efabd188dfcf7b8b79250f12d2ffbc5ce
Maksim Bogatov <maxbog@inbox.ru>
2020-02-25 20:51:14 +0300

    Урок 3

--- Services/WebStore.ServiceHosting/Startup.cs
+++ Services/WebStore.ServiceHosting/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -6,7 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using WebStore.DAL.Context;
+using WebStore.Data;
 using WebStore.Domain.Entities.Identity;
 using WebStore.Interfaces.Services;
 using WebStore.Services.Product;
@@ -24,11 +27,18 @@ namespace WebStore.ServiceHosting
         {
             services.AddDbContext<WebStoreContext>(opt =>
                 opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            //services.AddTransient<WebStoreContextInitializer>();
+            services.AddTransient<WebStoreContextInitializer>();
 
             services.AddIdentity<User, Role>()
                .AddEntityFrameworkStores<WebStoreContext>()
                .AddDefaultTokenProviders();
+
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "WebStore.API", Version = "v1" });
+                opt.IncludeXmlComments("WebStore.ServiceHosting.xml");
+                opt.IncludeXmlComments(@"bin\debug\netcoreapp2.2\WebStore.Domain.xml");
+            });
 
             services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
             services.AddScoped<IProductData, SqlProductData>();
@@ -40,12 +50,21 @@ namespace WebStore.ServiceHosting
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, WebStoreContextInitializer db)
         {
+            db.InitializeAsync().Wait();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(opt =>
+            {
+                opt.SwaggerEndpoint("/swagger/v1/swagger.json", "WebStore.API");
+                opt.RoutePrefix = string.Empty;
+            });
 
             app.UseMvc();
         }
---
+++ UI/WebStore/Infrastructure/AutoMapper/ViewModelMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Domain.ViewModels;
+using WebStore.Domain.ViewModels.Identity;
+
+namespace WebStore.Infrastructure.AutoMapper
+{
+    public class ViewModelMapping : Profile
+    {
+        public ViewModelMapping()
+        {
+            CreateMap<RegisterUserViewModel, User>()
+               //.ForMember(UserModel => UserModel.UserName, opt => opt.MapFrom(ViewModel => ViewModel.UserName))
+                ;
+
+        }
+    }
+}
